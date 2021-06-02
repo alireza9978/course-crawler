e8696f4f8fa80ea8fed7718d577664f6592e636b
Reza <reza.barzgar99@gmail.com>
2021-06-02 16:40:35 +0430

    editing prereq

--- main_brown.py
+++ main_brown.py
@@ -11,7 +11,7 @@ def extract_instructor_email():
         for temp_ins in browser.find_elements_by_class_name("instructor"):
             temp_result.append(temp_ins.find_element_by_css_selector(".instructor-detail h4 a").text)
         return temp_result
-    except:
+    except Exception as e:
         return None
 
 
@@ -21,18 +21,28 @@ def extract_instructor_name():
         for temp_ins in browser.find_elements_by_class_name("instructor"):
             temp_result.append(temp_ins.find_element_by_css_selector(".instructor-detail a").text)
         return temp_result
-    except:
+    except Exception as e:
+        return None
+
+
+def extract_prerequisite():
+    try:
+        temp_result = []
+        for temp_ins in browser.find_elements_by_css_selector(".prereq a"):
+            temp_result.append(temp_ins.text)
+        return temp_result
+    except Exception as e:
         return None
 
 
 def extract_courses_info(department_name):
     course_dict = {
-        "Course title": browser.find_element_by_xpath("/html/body/main/div[3]/div/div[2]/div[1]/div[2]").text,
+        "Course title": browser.find_element_by_css_selector(".text.col-8.detail-title.text--huge").text,
         "Department": department_name,
         "Abbreviation": "BROWN",
         "Professor": extract_instructor_name(),
         "Professor Homepage": extract_instructor_email(),
-        "Prerequisite": "as",
+        "Prerequisite": extract_prerequisite(),
         "Description": "Description",
         "References": "References"
     }
@@ -42,13 +52,13 @@ def extract_courses_info(department_name):
 
 def clrawl_department(name):
     browser.implicitly_wait(1)
-    panel = browser.find_element_by_xpath("/html/body/main/div[2]/div/div[3]")
+    panel = browser.find_elements_by_class_name("panel__body")[1]
     for temp_course in panel.find_elements_by_class_name("result--group-start")[:-1]:
         temp_course.find_element_by_class_name("result__title").click()
         extract_courses_info(name)
 
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path=r'C:\geckodriver.exe')
 browser.get(start)
 browser.find_element_by_xpath("//select[@id='crit-srcdb']/option[text()='Spring 2021']").click()
 
@@ -59,10 +69,12 @@ select_box = Select(select_box_for_text)
 
 options = [x for x in select_box_for_text.find_elements_by_tag_name("option")]
 temp = 0
-for i in options[1:]:
+for i in options[1:5]:
     browser.execute_script("document.getElementById('crit-dept').value = '{}'".format(i.get_attribute("value")))
     browser.execute_script("document.getElementById('search-button-sticky').click()")
     clrawl_department(i.text)
-    break
+    print(i)
 
-print(result)
+
+for item in result:
+    print(item)
