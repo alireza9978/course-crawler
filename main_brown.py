4bce61667ae5e2c5143f02f33fd8d50b673447c2
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-06-02 17:53:56 +0430

    add parallel mode for crawling with multiple browsers

--- main_brown.py
+++ main_brown.py
@@ -1,3 +1,7 @@
+import multiprocessing
+import time
+
+from joblib import Parallel, delayed
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
@@ -5,7 +9,7 @@ start = "https://cab.brown.edu/"
 result = []
 
 
-def extract_instructor_email():
+def extract_instructor_email(browser):
     try:
         temp_result = []
         for temp_ins in browser.find_elements_by_class_name("instructor"):
@@ -15,7 +19,7 @@ def extract_instructor_email():
         return None
 
 
-def extract_instructor_name():
+def extract_instructor_name(browser):
     try:
         temp_result = []
         for temp_ins in browser.find_elements_by_class_name("instructor"):
@@ -25,13 +29,17 @@ def extract_instructor_name():
         return None
 
 
-def extract_courses_info(department_name):
+def extract_references(browser, ):
+    pass
+
+
+def extract_courses_info(browser, department_name):
     course_dict = {
-        "Course title": browser.find_element_by_xpath("/html/body/main/div[3]/div/div[2]/div[1]/div[2]").text,
+        "Course title": browser.find_element_by_css_selector(".col-8").text,
         "Department": department_name,
         "Abbreviation": "BROWN",
-        "Professor": extract_instructor_name(),
-        "Professor Homepage": extract_instructor_email(),
+        "Professor": extract_instructor_name(browser),
+        "Professor Homepage": extract_instructor_email(browser),
         "Prerequisite": "as",
         "Description": "Description",
         "References": "References"
@@ -40,29 +48,35 @@ def extract_courses_info(department_name):
     result.append(course_dict)
 
 
-def clrawl_department(name):
-    browser.implicitly_wait(1)
-    panel = browser.find_element_by_xpath("/html/body/main/div[2]/div/div[3]")
-    for temp_course in panel.find_elements_by_class_name("result--group-start")[:-1]:
+def clrawl_department(browser, name):
+    time.sleep(0.5)
+    panel = browser.find_element_by_css_selector("div.panel__body:nth-child(3)")
+    for temp_course in panel.find_elements_by_css_selector("div.result")[:-1]:
         temp_course.find_element_by_class_name("result__title").click()
-        extract_courses_info(name)
+        time.sleep(0.5)
+        extract_courses_info(browser, name)
 
 
-browser = webdriver.Firefox()
-browser.get(start)
-browser.find_element_by_xpath("//select[@id='crit-srcdb']/option[text()='Spring 2021']").click()
+def new_crawler(i):
+    browser = webdriver.Firefox()
+    browser.get(start)
+    browser.find_element_by_xpath("//select[@id='crit-srcdb']/option[text()='Spring 2021']").click()
 
-select_box_for_text = browser.find_element_by_xpath("//select[@id='crit-dept']")
-select_box = Select(select_box_for_text)
-# if your select_box has a name.. why use xpath?.....
-# this step could use either xpath or name, but name is sooo much easier.
+    select_box_for_text = browser.find_element_by_xpath("//select[@id='crit-dept']")
+    select_box = Select(select_box_for_text)
+    # if your select_box has a name.. why use xpath?.....
+    # this step could use either xpath or name, but name is sooo much easier.
 
-options = [x for x in select_box_for_text.find_elements_by_tag_name("option")]
-temp = 0
-for i in options[1:]:
-    browser.execute_script("document.getElementById('crit-dept').value = '{}'".format(i.get_attribute("value")))
+    options = [x for x in select_box_for_text.find_elements_by_tag_name("option")]
+    option = options[i + 1]
+    browser.execute_script("document.getElementById('crit-dept').value = '{}'".format(option.get_attribute("value")))
     browser.execute_script("document.getElementById('search-button-sticky').click()")
-    clrawl_department(i.text)
-    break
+    clrawl_department(browser, option.text)
 
-print(result)
+    browser.close()
+
+    return result
+
+
+result_list = Parallel(n_jobs=1)(delayed(new_crawler)(i) for i in range(1, 10))
+print(result_list)
